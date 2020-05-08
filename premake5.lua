ad474468dc6527938a43001cd0e7594d42db7014
Serhii Herashchenko <sergsy.gerashenko@gmail.com>
2020-05-08 23:18:41 +0300

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,68 @@
+workspace "Sorting"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    platforms
+    {
+        "Win64"
+    }
+
+    flags
+    {
+        "MultiProcessorCompile"
+    }
+
+    TargetDir = "%{wks.location}/bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" 
+    OutputDir = "%{wks.location}/bin/tmp/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    project "Sorting"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++17"
+        
+        objdir (OutputDir .. "/%{prj.name}")
+        targetdir (TargetDir)
+
+        vpaths {
+            ["Includes"] = "include/**.h",
+            ["Sources"] = "src/**.cpp"
+        }
+
+        includedirs {"include"}
+
+    filter {"configurations:Debug"}
+        defines 
+        {
+            "DEBUG"
+        }
+        symbols "On"
+        runtime "Debug"
+
+    filter {"configurations:Release"}
+        defines 
+        {
+            "NDEBUG"
+            , "RELEASE"
+        }
+        optimize "On"
+        runtime "Release"
+
+    filter {"configurations:Release"}
+        defines 
+        {
+            "NDEBUG"
+            , "DIST"
+        }
+        optimize "On"
+        runtime "Release"
+        targetdir "bin"
+
+    filter {"platforms:Win64"}
+        systemversion "latest"
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello from: " << argv[0] << "!\n";
+    return EXIT_SUCCESS;
+}
